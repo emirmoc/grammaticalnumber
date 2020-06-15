8255553def6c009e681b4603b906962c627f0b15
emirmoc <emirmoc@users.noreply.github.com>
2020-06-15 11:27:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+const singularValue = 'singular';
+const paucalValue = 'paucal';
+const pluralValue = 'plural';
+
 function determineGrammaticalNumber(value) {
-    if (value < 0) {
-      value = -value;
-    }
-
-    const singularValue = 'singular';
-    const paucalValue = 'paucal';
-    const pluralValue = 'plural';
-
     // In Slavic languages:
     // Singular numbers end with 1, except when they end with 11
     // Paucal numbers end with 2, 3, 4, except when they end with 12, 13, 14
     // Plural numbers end with 0, 5-19
+    
+    // Making sure logic works for negative values
+    if (value < 0) {
+      value = -value;
+    }
 
     const ultimateDigit = value % 10;
     const penultimateDigit = value < 10 ? 0 : Math.floor(value / 10 % 10);
@@ -23,3 +24,15 @@ function determineGrammaticalNumber(value) {
       return pluralValue;
     }
   }
+
+function isSingular(value) {
+    return determineGrammaticalNumber(value) == singularValue;
+}
+
+function isPaucal(value) {
+    return determineGrammaticalNumber(value) == paucalValue;
+}
+
+function isPlural(value) {
+    return determineGrammaticalNumber(value) == pluralValue;
+}
